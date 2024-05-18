e1c7430d7530cb0938b8d0c2b8015dcdf144d2ac
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2024-05-18 10:24:31 -0300

    try to install org to fix build.

--- build-site.el
+++ build-site.el
@@ -29,6 +29,10 @@
   :pin "melpa-stable"
   :ensure t)
 
+(use-package org
+  :pin "melpa-stable"
+  :ensure t)
+
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
